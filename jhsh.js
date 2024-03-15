dacfda514e1060ce022fa77293111162da6381ee
Xh1275 <105969277+Xh1275@users.noreply.github.com>
2024-03-16 01:02:16 +0800

    更新 jhsh.js

--- jhsh.js
+++ jhsh.js
@@ -90,23 +90,23 @@ if (isGetCookie = typeof $request !== `undefined`) {
       $.msg($.name, '❌ 请先获取建行生活Cookie。');
       return;
     }
-    const date = new Date();
-    $.whichDay = date.getDay();
-    $.weekMap = {
-      0: "星期天",
-      1: "星期一",
-      2: "星期二",
-      3: "星期三",
-      4: "星期四",
-      5: "星期五",
-      6: "星期六",
-    };
-    if ($.whichDay === parseInt(skipDay)) {
-      let text = `今天是断签日[${$.weekMap[$.whichDay]}], 跳过签到任务。`
-      console.log(text);
-      message += text;
-      return;
-    }
+    //const date = new Date();
+    //$.whichDay = date.getDay();
+    //$.weekMap = {
+      //0: "星期天",
+      //1: "星期一",
+      //2: "星期二",
+      //3: "星期三",
+      //4: "星期四",
+      //5: "星期五",
+      //6: "星期六",
+    //};
+    //if ($.whichDay === parseInt(skipDay)) {
+      //let text = `今天是断签日[${$.weekMap[$.whichDay]}], 跳过签到任务。`
+      //console.log(text);
+      //message += text;
+      //return;
+    //}
     console.log(`\n共有[${bodyArr.length}]个建行生活账号\n`);
     await getLatestVersion();  // 获取版本信息
     for (let i = 0; i < bodyArr.length; i++) {
